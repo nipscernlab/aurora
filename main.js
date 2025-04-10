7ef62300cd4c1d28903775c7d29ff3de5e2ecce5
nipscern <nipscernlab@gmail.com>
2025-04-10 17:48:49 -0300

    v2.30.0
    v2.30.0

--- main.js
+++ main.js
@@ -220,7 +220,7 @@ ipcMain.on("open-prism-window", (event, svgPath) => {
       contextIsolation: true,
       nodeIntegration: false
     },
-    autoHideMenuBar: false,
+    autoHideMenuBar: true,
     frame: true
   });
 
@@ -312,7 +312,7 @@ async function createMainWindow() {
   mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
-    autoHideMenuBar: false,
+    autoHideMenuBar: true,
     icon: path.join(__dirname, 'assets/icons/aurora_borealis-2.ico'),
     webPreferences: {
       contextIsolation: true,

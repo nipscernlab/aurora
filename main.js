e2a01e6f46b96d85794cd5b3a3c1607020f76915
nipscern <nipscernlab@gmail.com>
2025-05-23 14:35:34 -0300

    v3.4.0
    v3.4.0

--- main.js
+++ main.js
@@ -142,7 +142,7 @@ function createSettingsWindow() {
     resizable: false,
     minimizable: false,
     maximizable: false,
-    autoHideMenuBar: false,
+    autoHideMenuBar: true,
     frame: false,
     webPreferences: {
       nodeIntegration: true,
@@ -315,7 +315,7 @@ async function createMainWindow() {
   mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
-    autoHideMenuBar: false,
+    autoHideMenuBar: true,
     icon: path.join(__dirname, 'assets/icons/aurora_borealis-2.ico'),
     webPreferences: {
       contextIsolation: true,
